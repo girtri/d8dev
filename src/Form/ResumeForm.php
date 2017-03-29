0b79e1d905b578e8008afd047dc0c08e59aafac4
gerry <girtri2@yahoo.it>
2017-03-30 00:44:05 +0200

    Select field from taxonomy: esempio con query su db e con funzione di drupal!

--- src/Form/ResumeForm.php
+++ src/Form/ResumeForm.php
@@ -62,6 +62,37 @@ class ResumeForm extends FormBase
       '#title' => t('Send me a copy of the application.'),
     );
 
+    //$terms = \Drupal\taxonomy\TermStorage::loadTree("classifica");
+    $vid = "classifica";
+    $parent = 0; $max_depth = NULL; $load_entities = FALSE;
+    
+    $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vid, $parent, $max_depth, $load_entities);
+    //$terms2 =\Drupal::service('entity_type.manager')->getStorage("taxonomy_term")->loadTree($vid, $parent = 0, $max_depth = NULL, $load_entities = FALSE);
+
+    $options = array();
+    foreach($terms  as $term) {
+      $options[$term->tid] = $term->name;
+    }
+
+    //$categorie = db_query("SELECT tid, name FROM {taxonomy_term_field_data} WHERE vid = 'classifica'")->fetchAll();
+
+    $form['classifica'] = array (
+      '#type' => 'select',
+      '#title' => ('Classifica'),
+      '#default_value' => 9,
+      '#options' => $options,
+    );
+
+    /*
+    $form['classifica2'] = array(
+      '#title' => 'La mia classifica',
+      '#type' => 'taxonomy_term',
+      '#vocabulary' => 'classifica',
+      '#parent_tid' => 0,
+      '#value_key' => 'tid',
+    );
+    */
+
     $form['actions']['#type'] = 'actions';
     
     $form['actions']['submit'] = array(
